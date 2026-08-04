a72b746dfb7c23e939d0d504a622fc0ee83111c0
jeffryesanchez <jeffryesanchez@gmail.com>
2026-08-04 14:15:03 -0700

    cambios al filtro

--- src/pages/Vehiculos/Vehiculos.tsx
+++ src/pages/Vehiculos/Vehiculos.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { HStack, Box, Heading, Text, VStack, Button, Select } from '@chakra-ui/react'
+import { HStack, Box, Heading, Text, VStack, Button, Select, Input, ButtonGroup } from '@chakra-ui/react'
 import type { Vehiculos } from '../../module/vehiculosTipo'
 import { getVehiculos } from '../../services/VehiculosServices'
 import VehiculosCard from './componentes/VehiculosCard'
@@ -41,14 +41,37 @@ const VehiculosPage = () => {
     return (
         <NavLayout>
             <Box bg="gray.900" color="white" minH="100vh" py="40px" px="20px">
-                <HStack>
-                    <VStack>
-                        <Select value={marca} onChange={handleSelectChange}>
-                            <option>Bentley</option>
-                            <option>BMW</option>
-                            <option>Ferrari</option>
-                            <option>Mercedes</option>
-                        </Select>
+                <HStack align="start">
+                    <VStack align="start">
+                        <Heading>Filtros</Heading>
+                        <VStack align="start" bgColor="gray.800" padding="20px" gap="2em">
+                            <VStack align="start">
+                                <Text fontWeight="bold">Browse Our Premium Inventory Selection</Text>
+                                <Text color="gray" fontStyle="italic">{autosFiltrados.length} In Stock</Text>
+                                <HStack>
+                                    <Input placeholder="Buscar por modelo" />
+                                    <Button colorScheme="purple">Buscar</Button>
+                                </HStack>
+                            </VStack>
+                            <VStack>
+                                <Text color="gray" fontStyle="italic">Selecciona una marca para filtrar los vehículos</Text>
+                                <Select value={marca} onChange={handleSelectChange}>
+                                    <option>Bentley</option>
+                                    <option>BMW</option>
+                                    <option>Ferrari</option>
+                                    <option>Mercedes</option>
+                                </Select>
+                            </VStack>
+                            <VStack align="start">
+                                <Text color="gray" fontStyle="italic">Selecciona Cantidad de Puertas</Text>
+                                <ButtonGroup>
+                                    <Button colorScheme="purple" variant="outline">1</Button>
+                                    <Button colorScheme="purple" variant="outline">2</Button>
+                                    <Button colorScheme="purple" variant="outline">3</Button>
+                                    <Button colorScheme="purple" variant="outline">4</Button>
+                                </ButtonGroup>
+                            </VStack>
+                        </VStack>
                     </VStack>
                     <VStack spacing={8} maxW="1200px" m="0 auto">
                         <VStack spacing={2} textAlign="center">
